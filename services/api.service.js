c406bebbe740934ebffb7a364b16418e2ec30c0d
Alex Khomenko <sasvsha@gmail.com>
2021-11-21 01:04:02 +0100

    Add env variable token

--- services/api.service.js
+++ services/api.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getKeyValue } from "./storage.service.js";
 
 const getWeather = async (city) => {
-	const token = await getKeyValue("token");
+	const token = process.env.TOKEN ?? (await getKeyValue("token"));
 	if (!token) {
 		throw new Error(
 			"API key not set! You can set it with -t [API_KEY] command in CLI"
